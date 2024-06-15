d16e4a11249b824147a31f3a5bd84c82a79367bc
orca-pet3910 <milosz.libiszewski@gmail.com>
2024-06-15 20:49:24 +0200

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,10 @@
+@echo off
+set "smp=%appdata%\microsoft\windows\start menu\programs"
+cd %~dp0
+echo If you want to proceed with installation, press any key. If you close this window, the installation won't begin. Installation path: "%userprofile%\Notepy"
+pause > nul
+echo Installing...
+mkdir "%userprofile%\Notepy"
+copy notepy_en "%userprofile%\Notepy"
+copy notepy_pl "%userprofile%\Notepy"
+echo Error code (0 means everything is okay, if it isn't, use "certutil /error %errorlevel%"): %errorlevel%
